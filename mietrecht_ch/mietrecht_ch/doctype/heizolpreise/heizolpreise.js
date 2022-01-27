5ef4efd384c673a3a4054c5893a38b82d716a8d0
Benoit Potty <benoit.potty@liip.ch>
2022-01-27 14:43:06 +0000

    Set first day of the month as default for new Document

--- mietrecht_ch/mietrecht_ch/doctype/heizolpreise/heizolpreise.js
+++ mietrecht_ch/mietrecht_ch/doctype/heizolpreise/heizolpreise.js
@@ -2,7 +2,10 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Heizolpreise', {
-	// refresh: function(frm) {
-
-	// }
+	onload: function(frm) {
+		if (!frm.doc.monat) {
+			var today = new Date();
+			frm.set_value('monat', new Date(today.getFullYear(), today.getMonth(), 1));
+		}
+	}
 });
