e104b5bc49a6e4a1fa76a6e9bb0d49394fd23934
Benoit Potty <benoit.potty@liip.ch>
2022-01-27 14:51:07 +0000

    Set first day of the month as default name for new Document

--- mietrecht_ch/mietrecht_ch/doctype/heizolpreise/heizolpreise.js
+++ mietrecht_ch/mietrecht_ch/doctype/heizolpreise/heizolpreise.js
@@ -5,7 +5,9 @@ frappe.ui.form.on('Heizolpreise', {
 	onload: function(frm) {
 		if (!frm.doc.monat) {
 			var today = new Date();
-			frm.set_value('monat', new Date(today.getFullYear(), today.getMonth(), 1));
+			var begginingOfTheMonth = new Date(today.getFullYear(), today.getMonth(), 1);
+			var monatString = `${begginingOfTheMonth.getFullYear()}-${(begginingOfTheMonth.getMonth() + 1).toString().padStart(2, '0')}-${begginingOfTheMonth.getDay().toString().padStart(2, '0')}`
+			frm.set_value('monat', monatString );
 		}
 	}
 });
