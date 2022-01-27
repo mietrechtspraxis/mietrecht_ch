3f6193df456ef851642ee86bebc6485d003f884f
Benoit Potty <benoit.potty@liip.ch>
2022-01-27 15:23:57 +0000

    MIET-43: Fix wrong date

--- mietrecht_ch/mietrecht_ch/doctype/heizolpreise/heizolpreise.js
+++ mietrecht_ch/mietrecht_ch/doctype/heizolpreise/heizolpreise.js
@@ -6,7 +6,7 @@ frappe.ui.form.on('Heizolpreise', {
 		if (!frm.doc.monat) {
 			var today = new Date();
 			var begginingOfTheMonth = new Date(today.getFullYear(), today.getMonth(), 1);
-			var monatString = `${begginingOfTheMonth.getFullYear()}-${(begginingOfTheMonth.getMonth() + 1).toString().padStart(2, '0')}-${begginingOfTheMonth.getDay().toString().padStart(2, '0')}`
+			var monatString = `${begginingOfTheMonth.getFullYear()}-${(begginingOfTheMonth.getMonth() + 1).toString().padStart(2, '0')}-01`;
 			frm.set_value('monat', monatString );
 		}
 	}
