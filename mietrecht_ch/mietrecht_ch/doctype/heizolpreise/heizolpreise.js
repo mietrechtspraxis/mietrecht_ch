a87200bc3acb1028e594bc9eab6e50aa114d9bd9
Benoit Potty <benoit.potty@liip.ch>
2022-01-28 08:46:34 +0000

    MIET-43: Last month is selected by default

--- mietrecht_ch/mietrecht_ch/doctype/heizolpreise/heizolpreise.js
+++ mietrecht_ch/mietrecht_ch/doctype/heizolpreise/heizolpreise.js
@@ -5,8 +5,8 @@ frappe.ui.form.on('Heizolpreise', {
 	onload: function(frm) {
 		if (!frm.doc.monat) {
 			var today = new Date();
-			var begginingOfTheMonth = new Date(today.getFullYear(), today.getMonth(), 1);
-			var monatString = `${begginingOfTheMonth.getFullYear()}-${(begginingOfTheMonth.getMonth() + 1).toString().padStart(2, '0')}-01`;
+			var begginingOfLastMonth = new Date(today.getFullYear(), today.getMonth() - 1, 1);
+			var monatString = `${begginingOfLastMonth.getFullYear()}-${(begginingOfLastMonth.getMonth() + 1).toString().padStart(2, '0')}-01`;
 			frm.set_value('monat', monatString );
 		}
 	}
