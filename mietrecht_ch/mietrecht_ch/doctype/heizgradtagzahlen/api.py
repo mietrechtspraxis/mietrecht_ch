89f84ca0298276f6afe59f5dbd9e53be6d99111a
Benoit Potty <benoit.potty@liip.ch>
2022-02-23 16:17:31 +0000

    MIET-44 : Implement get_sum_for_period

--- mietrecht_ch/mietrecht_ch/doctype/heizgradtagzahlen/api.py
+++ mietrecht_ch/mietrecht_ch/doctype/heizgradtagzahlen/api.py
@@ -1,12 +1,12 @@
 import frappe
 from mietrecht_ch.models.calculatorMasterResult import CalculatorMasterResult
 from mietrecht_ch.models.calculatorResult import CalculatorResult
-from mietrecht_ch.utils.dateUtils import buildFullDate
+from mietrecht_ch.utils.dateUtils import buildFullDate, swapDateIfNeeded
 from mietrecht_ch.utils.queryExecutor import execute_query
 
 @frappe.whitelist(allow_guest=True)
 def get_sum_for_month(location, year, month):
-    coldDays  = execute_query("""SELECT `monat` as `month`, `{location}` as `days`
+    coldDays  = execute_query("""SELECT `monat` as `month`, `{location}` as `sum`
                                 FROM `tabHeizgradtagzahlen` 
                                 WHERE `monat` LIKE '{date}';""".format(location=location, date=buildFullDate(year, month)))
     
@@ -14,5 +14,24 @@ def get_sum_for_month(location, year, month):
 
     return CalculatorMasterResult(
         {'location':location, 'year':year, 'month':month},
+        [calculatorResult]
+    )
+
+@frappe.whitelist(allow_guest=True)
+def get_sum_for_period(location, fromYear, fromMonth, toYear, toMonth):
+
+    fromFull = buildFullDate(fromYear, fromMonth)
+    toFull = buildFullDate(toYear, toMonth)
+
+    fromFull, toFull = swapDateIfNeeded(fromFull, toFull)
+    coldDays  = execute_query("""SELECT `monat` as `month`, SUM(`{location}`) as `sum`
+                                FROM `tabHeizgradtagzahlen`
+                                WHERE `monat` BETWEEN '{fromFull}' AND '{toFull}';"""
+                                .format(location=location, fromFull=fromFull, toFull=toFull))
+    
+    calculatorResult = CalculatorResult(coldDays[0] if coldDays else None, None)
+
+    return CalculatorMasterResult(
+        {'location':location, 'fromYear':fromYear, 'fromMonth':fromMonth, 'toYear':toYear, 'toMonth':toMonth},
         [calculatorResult]
     )
