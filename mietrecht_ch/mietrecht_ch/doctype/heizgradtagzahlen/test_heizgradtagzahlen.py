3e7317905caf6a687a9fe213e391f935d1e5b199
Margaux Dupuy <margaux.dupuy@liip.ch>
2022-03-01 16:03:30 +0000

    Review BE: add testing and code linters

--- mietrecht_ch/mietrecht_ch/doctype/heizgradtagzahlen/test_heizgradtagzahlen.py
+++ mietrecht_ch/mietrecht_ch/doctype/heizgradtagzahlen/test_heizgradtagzahlen.py
@@ -3,8 +3,11 @@
 # See license.txt
 from __future__ import unicode_literals
 
-# import frappe
-import unittest
 
-class TestHeizgradtagzahlen(unittest.TestCase):
-	pass
+class TestHeizgradtagzahlen():
+	def setup(self):
+		self.data1 = 'data1'
+		self.data2 = 'data2'
+
+	def test_data_not_equals(self):
+		assert self.data1 != self.data2
