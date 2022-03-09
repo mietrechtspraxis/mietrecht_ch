30e827b75d56a5ba53895c9ae41e272b8fba51ea
Benoit Potty <benoit.potty@liip.ch>
2022-03-09 10:07:22 +0000

    MIET-133: Fake data from API

--- mietrecht_ch/mietrecht_ch/doctype/lebensdauer/api.py
+++ mietrecht_ch/mietrecht_ch/doctype/lebensdauer/api.py
@@ -1,10 +1,39 @@
 import frappe
+from mietrecht_ch.models.calculatorMasterResult import CalculatorMasterResult
+from mietrecht_ch.models.calculatorResult import CalculatorResult
+from mietrecht_ch.models.lebensdauer import LebensdauerEntry, LebensdauerRemedy, LebensdauerResult
 
 @frappe.whitelist(allow_guest=True)
 def get_all_by_group(groupId):
-    return { 'params': {'groupId':groupId}}
+    return CalculatorMasterResult( 
+        {'groupId':groupId}, 
+        CalculatorResult(get_fake_data(), None)
+    )
 
 
 @frappe.whitelist(allow_guest=True)
 def get_all_by_keyword(keyword):
-    return { 'params': {'keyword':keyword}}
+    return CalculatorMasterResult( 
+        {'keyword':keyword}, 
+        CalculatorResult(get_fake_data(), None)
+    )
+
+
+def get_fake_data():
+    agregateChildren = [
+        LebensdauerEntry('für Warmluftcheminée', lifetime=20),
+        LebensdauerEntry('zur Wärmerückgewinnung', lifetime=20),
+    ]
+
+    chemineeChildren = [
+        LebensdauerEntry('Cheminée, Cheminéeofen, Schwedenofen', lifetime=25),
+        LebensdauerEntry('Schamottsteinauskleidung', lifetime=15, remedy=LebensdauerRemedy('Neuauskleidung', 'm²', 800))
+    ]
+
+    entries = [
+        LebensdauerEntry('Aggregate', agregateChildren),
+        LebensdauerEntry('Cheminéeabschluss', comment="Metallgitter, Glas"),
+        LebensdauerEntry('Cheminées', chemineeChildren),
+        LebensdauerEntry('Ventilator', comment='Zu Rauchabzug', lifetime=20),
+    ]
+    return LebensdauerResult('Cheminée', entries)
--- mietrecht_ch/models/lebensdauer.py
+++ mietrecht_ch/models/lebensdauer.py
@@ -3,12 +3,12 @@ from typing import List
 
 class LebensdauerRemedy(dict):
     def __init__(self, label:str, unit: str, price:number):
-        dict.__init__(self, label, unit, price)
+        dict.__init__(self, label=label, unit=unit, price=price)
 
 class LebensdauerEntry(dict):
     def __init__(self, label, children: list = None, lifetime:number = None, remedy: LebensdauerRemedy = None, comment: str = None):
-        dict.__init__(self, label, children, lifetime, remedy, comment)
+        dict.__init__(self, label=label, children=children, lifetime=lifetime, remedy=remedy, comment=comment)
 
 class LebensdauerResult(dict):
-    def __init__(self, groupeName: str, entries: List[LebensdauerEntry]= None):
-        dict.__init__(self, groupeName, entries)
+    def __init__(self, groupName: str, entries: List[LebensdauerEntry]= None):
+        dict.__init__(self, groupName=groupName, entries=entries)
