994c7eb01965214a6826ecee05bcf73e727b8cc8
Benoit Potty <benoit.potty@liip.ch>
2022-03-10 10:13:35 +0000

    MIET-133: Remove unused Lebensdauer Doctype

--- mietrecht_ch/mietrecht_ch/doctype/lebensdauer/lebensdauer.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2022, mietrecht.ch and contributors
-// For license information, please see license.txt
-
-frappe.ui.form.on('Lebensdauer', {
-	// refresh: function(frm) {
-
-	// }
-});
