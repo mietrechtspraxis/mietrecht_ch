cdbe6ba662e0a68e8dc237564b5fee903ca5e858
Benoit Potty <benoit.potty@liip.ch>
2022-03-13 07:38:51 +0000

    MIET-131: Add API and fix SQL issues with column names

---
+++ mietrecht_ch/mietrecht_ch/doctype/aktualisierungsdaten/api.py
@@ -0,0 +1,18 @@
+from warnings import filters
+import frappe
+import datetime 
+
+@frappe.whitelist(allow_guest=True)
+def get_all():
+    
+    today = datetime.datetime.now()
+    delay = datetime.timedelta(days = 3)
+    threeDaysAgo = today - delay
+    
+    return frappe.get_all(
+        'Aktualisierungsdaten', 
+        fields=['index_date' ,'update_date'],
+        # filters = [
+        #      ["update", ">=", threeDaysAgo],
+        # ]
+    )
