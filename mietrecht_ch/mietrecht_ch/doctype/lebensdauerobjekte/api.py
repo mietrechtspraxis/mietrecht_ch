4221324f13c6f64ebf81a64eb8cbcad4a3c43c48
Benoit Potty <benoit.potty@liip.ch>
2022-03-14 09:51:49 +0000

    MIET-133: Prepare API

--- mietrecht_ch/mietrecht_ch/doctype/lebensdauerobjekte/api.py
+++ mietrecht_ch/mietrecht_ch/doctype/lebensdauerobjekte/api.py
@@ -1,3 +1,6 @@
+from ast import List
+from dataclasses import field, fields
+from traceback import print_tb
 import frappe
 from mietrecht_ch.models.calculatorMasterResult import CalculatorMasterResult
 from mietrecht_ch.models.calculatorResult import CalculatorResult
@@ -5,11 +8,73 @@ from mietrecht_ch.models.lebensdauer import LebensdauerEntry, LebensdauerRemedy,
 
 @frappe.whitelist(allow_guest=True)
 def get_all_by_group(groupId):
-    return CalculatorMasterResult( 
-        {'groupId':groupId}, 
-        [CalculatorResult(get_fake_data(), None)]
+
+    groups: list = frappe.get_all(
+        'LebensdauerGruppe',
+        fields= ['label', 'value'],
+        filters= {
+            "value": ("like", groupId)
+        }
     )
 
+    if len(groups) == 0:
+        return CalculatorMasterResult( 
+            {'groupId':groupId}, 
+            [CalculatorResult([], None)]
+    )
+    
+    group = groups[0]
+
+    groupObjects = frappe.get_all(
+        'LebensdauerObjekte',
+        fields= ['*'],
+        filters= {
+            "group": ("like", group.label)
+        }
+    )
+
+    groupEntries = []
+
+    # TODO : TROUVER les parents d'abord, puis les enfant
+    #   Soit trier en code pour mettre les parents en premier
+    #
+    #   Sinon : filter pour sortir tous les parents puis tous les enfants
+    #
+    for objekte in groupObjects:
+
+        if objekte['child_object']:
+            __insert_child_object__(groupEntries, objekte)
+        else:
+            __insert_parent_object__(groupEntries, objekte)
+
+    # END TODO
+
+    lebensdauerResult = LebensdauerResult(group.label, groupEntries)
+
+    return CalculatorMasterResult( 
+        {'groupId':groupId}, 
+        [CalculatorResult([lebensdauerResult], None)]
+    )
+
+def __insert_child_object__(groupEntries, obj):
+    parent = next(x for x in groupEntries if x['label'] == obj['object'])
+
+    if not parent.children:
+        parent.children = []
+
+    parent.children.append(__createEntry__(obj))
+
+def __createEntry__(obj):
+    return LebensdauerEntry(obj['object'], None, obj['lifetime'], __get_remedy__(obj), obj['comment'])
+
+def __insert_parent_object__(groupEntries, obj):
+    groupEntries.append(__createEntry__(obj))
+    
+def __get_remedy__(obj):
+    if (obj.remedy != ""):
+        return LebensdauerRemedy(obj['remedy'], obj['unit'], obj['price'])
+    
+    return None
 
 @frappe.whitelist(allow_guest=True)
 def get_all_by_keyword(keyword):
