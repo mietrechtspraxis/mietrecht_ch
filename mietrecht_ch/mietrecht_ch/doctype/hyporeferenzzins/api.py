c79fd4da83233522f879680b22f155fe111df2a8
Benoit Potty <benoit.potty@liip.ch>
2022-03-18 09:45:25 +0000

    MIET-147: Encapsulate in CalculatorResult

--- mietrecht_ch/mietrecht_ch/doctype/hyporeferenzzins/api.py
+++ mietrecht_ch/mietrecht_ch/doctype/hyporeferenzzins/api.py
@@ -1,6 +1,8 @@
 from numbers import Number
 from warnings import filters
 import frappe
+from mietrecht_ch.models.calculatorMasterResult import CalculatorMasterResult
+from mietrecht_ch.models.calculatorResult import CalculatorResult
 from mietrecht_ch.utils.queryExecutor import execute_query
 
 @frappe.whitelist(allow_guest=True)
@@ -8,7 +10,7 @@ def get_index_by_month(year: Number, month: Number, canton:str = 'CH'):
     
     print("Params : year:{} month:{} canton:{}".format(year, month, canton))
     
-    result = execute_query("""SELECT canton, publish_date, interest 
+    closestIndex = execute_query("""SELECT publish_date, interest 
                               FROM tabHypoReferenzzins 
                               WHERE (canton = '{canton}' OR canton = 'CH')
                               AND publish_date < LAST_DAY('{year}-{month}-01')
@@ -16,5 +18,19 @@ def get_index_by_month(year: Number, month: Number, canton:str = 'CH'):
                               LIMIT 1
                               """
                               .format(canton=canton, year=year, month=month))
+    result = None
+
+    if len(closestIndex) > 0:
+        result = {
+            'interest' : {
+                'publish_date': closestIndex[0].publish_date,
+                'interest': closestIndex[0].interest
+            }
+        }
     
-    return result
+    calculatorResult = CalculatorResult(result, None)
+
+    return CalculatorMasterResult(
+        {'canton':canton, 'year':year, 'month':month},
+        [calculatorResult]
+    )
