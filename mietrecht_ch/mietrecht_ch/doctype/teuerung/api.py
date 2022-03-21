5313043126938894c5dbac2da5811474066813ac
Noxter <david.planchon@liip.ch>
2022-03-21 13:26:42 +0000

    improvement of algorithm for landesindex feature and write swagger doc for landesindex

--- mietrecht_ch/mietrecht_ch/doctype/teuerung/api.py
+++ mietrecht_ch/mietrecht_ch/doctype/teuerung/api.py
@@ -4,7 +4,6 @@ from datetime import date
 from typing import List
 from webbrowser import get
 import frappe
-# from mietrecht_ch.models.teuerung import TeuerungEntry
 from mietrecht_ch.models.calculatorMasterResult import CalculatorMasterResult
 from mietrecht_ch.models.calculatorResult import CalculatorResult
 from mietrecht_ch.models.resultTableDescription import ResultTableDescription
@@ -17,11 +16,11 @@ import itertools
 @frappe.whitelist(allow_guest=True)
 def get_last_five_indexes():
 
-    lastFivePublishDates = execute_query(
+    last_five_publish_dates = execute_query(
         """select distinct(publish_date) from tabTeuerung where DAY(publish_date) = 1 order by publish_date desc LIMIT 5;""")
 
     inClause = ','.join(map(lambda x: "'{}'".format(
-        x['publish_date'].strftime('%Y-%m-%d')), lastFivePublishDates))
+        x['publish_date'].strftime('%Y-%m-%d')), last_five_publish_dates))
 
     indexes = execute_query(
         """select base_year, publish_date, value from tabTeuerung where publish_date IN ({inClause}) order by base_year DESC, publish_date""".format(inClause=inClause))
@@ -30,30 +29,32 @@ def get_last_five_indexes():
 
 
     base_year_integer = []
-    converted_base_year_integer = __create_integer__(indexes, base_year_integer)
+    converted_base_year_integer = __create_unique_basis_from_indexes__(indexes, base_year_integer)
 
-    result = dict()
+    result = []
     for x in converted_base_year_integer:
-        dictTemp = dict()
-        dictTemp['base_year'] = x
-        dictTemp['value'] = [y['value'] for y in indexes if y['base_year'] == str(x)]
-        result[str(x)] = dictTemp
+        listTemp = []
+        listTemp.append(x)
+        listTemp.extend([y['value'] for y in indexes if y['base_year'] == str(x)])
+        result.append(listTemp)
 
-    resultTableDescriptions = [
-        ResultTableDescription([last_five_months], "number")
-    ]
+    result_table_description_iterated = []
+    for x in last_five_months:
+        resultTableDescriptions = [
+            result_table_description_iterated.append(ResultTableDescription([x], "number"))
+        ]
 
-    resultTable = ResultTable(resultTableDescriptions, result)
+    resultTable = ResultTable(result_table_description_iterated, result)
 
     calculatorResult = CalculatorResult(None, resultTable)
 
     return CalculatorMasterResult(
-        {'teueroung': '1914'},
+        {None},
         [calculatorResult]
     )
 
 
-def __create_integer__(indexes, baseYearIntegers):
+def __create_unique_basis_from_indexes__(indexes, baseYearIntegers):
     for i in indexes:
         baseYearIntegers.append(int(i.base_year))
     return sorted(set(baseYearIntegers), key=None, reverse=True)
@@ -65,12 +66,3 @@ def __get_last_five_months__():
         now = now.replace(day=1, hour=0, minute=0, second=0, microsecond=0) - timedelta(days=1)
         result.append(now)
     return result
-
-# def __structured_last_five_months__(convertedBaseYearInteger, last_five_months):
-#     result_description = dict()
-#     for y in itertools.islice(convertedBaseYearInteger, 5):
-#         dict_description = dict()
-#         dict_description['base_year'] = [x for x in last_five_months]
-#         result_description[str(y)] = dict_description
-
-#     return result_description
