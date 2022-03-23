6bd439854f47e02f15628f2461c031e3e9e9bb9b
Benoit Potty <benoit.potty@liip.ch>
2022-03-23 16:55:58 +0000

    MIET-154 : Add zoom capability to API

--- mietrecht_ch/mietrecht_ch/doctype/hypothekarzins/api.py
+++ mietrecht_ch/mietrecht_ch/doctype/hypothekarzins/api.py
@@ -1,16 +1,22 @@
+from datetime import date
 from unittest import result
 import frappe
 from mietrecht_ch.models.calculatorMasterResult import CalculatorMasterResult
 from mietrecht_ch.models.calculatorResult import CalculatorResult
+from mietrecht_ch.utils.dateUtils import buildDatesInChronologicalOrder
 from mietrecht_ch.utils.queryExecutor import execute_query
 
 
 @frappe.whitelist(allow_guest=True)
-def get_dataset():
+def get_dataset(fromMonth = '01', fromYear = '1970', toMonth = '12', toYear = str(date.today().year)):
 
+    from_date, to_date = buildDatesInChronologicalOrder(fromYear, fromMonth, toYear, toMonth, toDay='31')
+    print(from_date)
+    print(to_date)
     db_objects = execute_query("""SELECT `date`, interest_rate, average FROM tabHypothekarzins
+                                WHERE `date` BETWEEN '{from_date}' AND '{to_date}'
                                 ORDER BY `date`
-                                """)
+                                """.format(from_date=from_date, to_date=to_date))
 
     return CalculatorMasterResult(
         {},
