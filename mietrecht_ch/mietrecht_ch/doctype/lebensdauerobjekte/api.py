63882410c2c65fc6cbf31ae816c1f731d537d13d
Benoit Potty <benoit.potty@liip.ch>
2022-03-23 09:36:42 +0000

    MIET-119: Return null result when nothing is found

--- mietrecht_ch/mietrecht_ch/doctype/lebensdauerobjekte/api.py
+++ mietrecht_ch/mietrecht_ch/doctype/lebensdauerobjekte/api.py
@@ -88,45 +88,19 @@ def __get_remedy__(obj):
 
 @frappe.whitelist(allow_guest=True)
 def get_all_by_keyword(keyword):
-    return CalculatorMasterResult(
-        {'keyword': keyword},
-        [CalculatorResult(get_fake_data(), None)]
+
+    groupObjects = frappe.get_all(
+        'LebensdauerObjekte',
+        fields=['*'],
+        filters={
+            "keywords": ("like", keyword)
+        }
     )
 
+    if len(groupObjects) == 0:
+        structured_result = None
 
-def get_fake_data():
-    agregateChildren = [
-        LebensdauerEntry('für Warmluftcheminée', lifetime=20),
-        LebensdauerEntry('zur Wärmerückgewinnung', lifetime=20),
-    ]
-
-    chemineeChildren = [
-        LebensdauerEntry('Cheminée, Cheminéeofen, Schwedenofen', lifetime=25),
-        LebensdauerEntry('Schamottsteinauskleidung', lifetime=15,
-                         remedy=LebensdauerRemedy('Neuauskleidung', 'm²', 800))
-    ]
-
-    chemineeEntries = [
-        LebensdauerEntry('Aggregate', agregateChildren),
-        LebensdauerEntry('Cheminéeabschluss',
-                         comment="Metallgitter, Glas", lifetime=20),
-        LebensdauerEntry('Cheminées', chemineeChildren),
-        LebensdauerEntry('Ventilator', comment='Zu Rauchabzug', lifetime=20),
-    ]
-
-    otherChildren = [
-        LebensdauerEntry('Kunststoft', lifetime=15,
-                         remedy=LebensdauerRemedy('Ersatz', 'Stk.', 75)),
-        LebensdauerEntry('Metall', lifetime=20,
-                         remedy=LebensdauerRemedy('Ersatz', 'Stk.', 75)),
-    ]
-
-    otherEntries = [
-        LebensdauerEntry(
-            'Abdeckungen zu Lüftungsanlagen/-gittern', otherChildren),
-    ]
-
-    return [
-        LebensdauerResult('Cheminée', chemineeEntries),
-        LebensdauerResult('Heizung / Lüftung / Klima', otherEntries),
-    ]
+    return CalculatorMasterResult(
+        {'keyword': keyword},
+        [CalculatorResult(structured_result, None)]
+    )
