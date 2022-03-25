4e077cdef2a5ea1cab4e7fb563c8a889633297af
Noxter <david.planchon@liip.ch>
2022-03-25 13:30:47 +0000

    create two api endpoints and add swagger documentation

--- mietrecht_ch/mietrecht_ch/doctype/teuerung/api.py
+++ mietrecht_ch/mietrecht_ch/doctype/teuerung/api.py
@@ -1,12 +1,14 @@
 from calendar import month
 from datetime import datetime, timedelta
+from queue import Empty
+from unittest import result
 import frappe
 from mietrecht_ch.models.calculatorMasterResult import CalculatorMasterResult
 from mietrecht_ch.models.calculatorResult import CalculatorResult
 from mietrecht_ch.models.resultRow import ResultRow
 from mietrecht_ch.models.resultTableDescription import ResultTableDescription
 from mietrecht_ch.models.resultTable import ResultTable
-from mietrecht_ch.models.teuerung import TeuerungInflationResult, TeuerungOldIndex, TeuerungNewIndex, FIELD_VALUE
+from mietrecht_ch.models.teuerung import TeuerungInflationResult, TeuerungOldIndex, TeuerungNewIndex, TeuerungLastRelevantIndexResult, FIELD_VALUE
 from mietrecht_ch.utils.queryExecutor import execute_query
 from mietrecht_ch.utils.dateUtils import buildFullDate, buildDatesInChronologicalOrder, swapDateIfNeeded
 
@@ -62,7 +64,8 @@ def get_inflation_for_period(basis: int, inflationRate: float, fromMonth: int, f
     values_from_sql_query = __get_values_from_sql_query__(
         basis, old_date_formatted, new_date_formatted)
 
-    results = __compute_result__(inflationRate, old_date_formatted, new_date_formatted, values_from_sql_query)
+    results = __compute_result__(
+        inflationRate, old_date_formatted, new_date_formatted, values_from_sql_query)
 
     calculatorResult = CalculatorResult(results, None)
 
@@ -72,9 +75,76 @@ def get_inflation_for_period(basis: int, inflationRate: float, fromMonth: int, f
         [calculatorResult]
     )
 
+@frappe.whitelist(allow_guest=True)
+def get_basis_by_index(index: int):
+    all_basis = frappe.get_all(
+        'Teuerung',
+        fields=['base_year', 'publish_date'],
+        filters=[
+                ["value", "=", index]
+        ]
+    )
+
+    results = __compute_all_basis_results__(all_basis, index)
+
+    result_table_description = [
+        ResultTableDescription('Basis', "number"),
+        ResultTableDescription('Monat/Jahr', "string")
+    ]
+
+    resultTable = ResultTable(result_table_description, results)
+
+    calculatorResult = CalculatorResult(None, resultTable)
+
+    return CalculatorMasterResult(
+        {'index': index},
+        [calculatorResult]
+    )
+
+@frappe.whitelist(allow_guest=True)
+def get_last_index_from_basis(basis, fromMonth, fromYear):
+
+    results = __last_relevant_index_result__(basis, fromMonth, fromYear)
+
+    calculatorResult = CalculatorResult(results, None)
+
+    return CalculatorMasterResult(
+        {'basis': basis, 'fromMonth': fromMonth, 'fromYear': fromYear},
+        [calculatorResult]
+    )
+
+def __last_relevant_index_result__(basis, fromMonth, fromYear):
+
+    date_formatted = buildFullDate(fromYear, fromMonth)
+
+    last_relevant_index = execute_query(
+        """select publish_date, base_year, value 
+            from tabTeuerung 
+            where base_year = '{basis}' and publish_date = '{date_formatted}'
+            order by publish_date desc limit 1
+            """
+        .format(basis=basis, date_formatted=date_formatted))
+
+    results = None
+    if last_relevant_index and len(last_relevant_index) == 1:
+        results = []
+        for i in last_relevant_index:
+            results = [TeuerungLastRelevantIndexResult(
+                i['base_year'], i['publish_date'], i['value'])]
+    return results
+
+
+def __compute_all_basis_results__(all_basis, index):
+    results = None
+    if index != '':
+        results = []
+        for b in all_basis:
+            results.append(ResultRow(b))
+    return results
+
 def __compute_result__(inflationRate, old_date_formatted, new_date_formatted, values_from_sql_query):
     results = None
-    if  values_from_sql_query and len(values_from_sql_query) == 2:
+    if values_from_sql_query and len(values_from_sql_query) == 2:
         old_index_value = values_from_sql_query[0][FIELD_VALUE]
         new_index_value = values_from_sql_query[1][FIELD_VALUE]
 
@@ -83,7 +153,7 @@ def __compute_result__(inflationRate, old_date_formatted, new_date_formatted, va
 
         results = __result_of_all_data__(
             old_date_formatted, old_index_value, new_date_formatted, new_index_value, rounded_inflation)
-            
+
     return results
 
 
--- mietrecht_ch/models/teuerung.py
+++ mietrecht_ch/models/teuerung.py
@@ -1,3 +1,5 @@
+from typing import List
+
 FIELD_VALUE = 'value'
 
 class TeuerungOldIndex(dict):
@@ -12,3 +14,7 @@ class TeuerungInflationResult(dict):
     def __init__(self, oldIndex: TeuerungOldIndex, newIndex: TeuerungNewIndex, inflation: float):
         dict.__init__(self, oldIndex=oldIndex,
                       newIndex=newIndex, inflation=inflation)
+
+class TeuerungLastRelevantIndexResult(dict):
+    def __init__(self, basis: float, fromMonth: float, fromYear: float):
+        dict.__init__(self, basis=basis, fromMonth=fromMonth, fromYear=fromYear)
