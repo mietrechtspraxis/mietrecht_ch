73e5a8661c93032defc808a04fcb2e9845057de0
noxter <david.planchon@liip.ch>
2022-06-20 11:38:52 +0000

    miet-188-radiobutton: included the manual and search option

--- mietrecht_ch/mietrecht_ch/doctype/rent/api.py
+++ mietrecht_ch/mietrecht_ch/doctype/rent/api.py
@@ -10,6 +10,7 @@ from mietrecht_ch.models.rent import CalculationValue, CostIncrease, CostLevel, 
 from mietrecht_ch.utils.inflation import __rounding_value__
 from mietrecht_ch.utils.hyporeferenzzinsUtils import __rent_pourcentage_calculation__
 from mietrecht_ch.models.exceptions.mietrechtException import BadRequestException
+from mietrecht_ch.utils.inflation import __round_inflation_number__
 
 
 @frappe.whitelist(allow_guest=True)
@@ -17,32 +18,35 @@ def compute_rent():
 
     payload = json.loads(frappe.request.data)
     rent = payload['rent']['rent']
+    inflation_rate = 100
+    canton = 'CH'
 
     # hypoReference
-    old_date_formatted_hypo, new_date_formatted_hypo, from_date_interest, to_date_interest, rent_pourcentage_change, rounded_calculation_rent_hypo = hypotekarzins_data(
+    from_interest, to_interest, rent_pourcentage_change, final_rent_hypo, old_date_formatted_hypo, new_date_formatted_hypo = hypotekarzins_data(
         payload, rent)
 
     result_hypothekarzinsen = CalculationValue(
-        to_date_interest, from_date_interest, rent_pourcentage_change, rounded_calculation_rent_hypo)
+        from_interest, to_interest, rent_pourcentage_change, final_rent_hypo)
 
     # Inflation
-    teuerung_result, teuerung_inflation, rounded_calculation_rent_teuerung, inflationRate, affected_date, new_date_formatted_teuerung = teuerung_data(
+    # return teuerung_data(payload)
+    from_index, at_index, teuerung_inflation, final_rent_calculation, index_basis, affected_date = teuerung_data(
         payload)
-
     results_teuerung = CalculationValue(
-        teuerung_result['oldIndex']['value'], teuerung_result['newIndex']['value'], teuerung_inflation, rounded_calculation_rent_teuerung)
+        from_index, at_index, teuerung_inflation, final_rent_calculation)
 
     # Kostensteigerungen
     start_day_date, end_day_date, cost_inflation, cost_value, flat_rate = general_costs_increase_data(
         payload, rent)
-
+    return general_costs_increase_data(
+        payload, rent)
     result_general_cost = CalculationValue(
         start_day_date, end_day_date, cost_inflation, cost_value)
 
     # Total
     # total in percent from all calculation
     rounded_total_in_percent, rounded_total_in_sfr = total_data(
-        rent_pourcentage_change, rounded_calculation_rent_hypo, teuerung_inflation, rounded_calculation_rent_teuerung, cost_inflation, cost_value)
+        rent_pourcentage_change, final_rent_hypo, teuerung_inflation, final_rent_calculation, cost_inflation, cost_value)
 
     result_total = CalculatedPercentage(
         rounded_total_in_percent, rounded_total_in_sfr)
@@ -58,10 +62,10 @@ def compute_rent():
 
     # CostLevel
     mortage_interest_rate = MortgageInterestRate(
-        new_date_formatted_hypo, 'CH', to_date_interest)
+        old_date_formatted_hypo, canton, from_interest)
     # Inflation
     data_inflation = Inflation(
-        new_date_formatted_teuerung, inflationRate, teuerung_result['newIndex']['value'], affected_date)
+        index_basis, inflation_rate, at_index, affected_date)
 
     # costIncrease
     cost_increase = CostIncrease(flat_rate, end_day_date)
@@ -147,12 +151,12 @@ def compute_rent():
     )
 
 
-def total_data(rent_pourcentage_change, rounded_calculation_rent_hypo, teuerung_inflation, rounded_calculation_rent_teuerung, cost_inflation, cost_value):
+def total_data(rent_pourcentage_change, final_rent_hypo, teuerung_inflation, rent_teuerung, cost_inflation, cost_value):
     total_in_percent = teuerung_inflation + rent_pourcentage_change + cost_inflation
     rounded_total_in_percent = __rounding_value__(total_in_percent)
     # total in sfr from all calculation
-    total_in_sfr = rounded_calculation_rent_hypo + \
-        rounded_calculation_rent_teuerung + cost_value
+    total_in_sfr = final_rent_hypo + \
+        rent_teuerung + cost_value
     rounded_total_in_sfr = __rounding_value__(total_in_sfr)
     return rounded_total_in_percent, rounded_total_in_sfr
 
@@ -227,25 +231,57 @@ def teuerung_data(payload):
     toMonth = payload['inflation']['next']['month']
     basis = payload['inflation']['basis']
     total_original = payload['rent']['rent'] + payload['rent']['extraRoom']
+    input_type = payload['inflation']['inputType']
+
     inflationRate: int = 40
 
-    old_date_formatted_teuerung, new_date_formatted_teuerung = buildDatesInChronologicalOrder(
-        fromYear, fromMonth, toYear, toMonth)
+    # old_date_formatted_teuerung, new_date_formatted_teuerung = buildDatesInChronologicalOrder(
+    #     fromYear, fromMonth, toYear, toMonth)
+
+    old_date_formatted_teuerung = buildFullDate(fromYear, fromMonth)
+    new_date_formatted_teuerung = buildFullDate(toYear, toMonth)
 
     values_from_sql_query = __get_values_from_sql_query__(
         basis, old_date_formatted_teuerung, new_date_formatted_teuerung)
+    index_basis = values_from_sql_query[1]['base_year']
+    affected_date = values_from_sql_query[1]['publish_date']
+
+    if input_type == 'search':
+        return __get_data_from_search_input__(total_original, inflationRate, old_date_formatted_teuerung, new_date_formatted_teuerung, values_from_sql_query, index_basis, affected_date)
+    return __get_data_from_manual_input__(payload, total_original, index_basis, affected_date)
+
+
+def __get_data_from_search_input__(total_original, inflationRate, old_date_formatted_teuerung, new_date_formatted_teuerung, values_from_sql_query, index_basis, affected_date):
+
     if values_from_sql_query and len(values_from_sql_query) == 2:
-        affected_date = values_from_sql_query[1]['publish_date']
         teuerung_result = __compute_result__(
             inflationRate, old_date_formatted_teuerung, new_date_formatted_teuerung, values_from_sql_query, rent=None)
+        from_index = teuerung_result['oldIndex']['value']
+        at_index = teuerung_result['newIndex']['value']
         teuerung_inflation = teuerung_result['inflation']
         calculation_rent_teuerung = __rent_calculation__(
             teuerung_result['inflation'], total_original)
-        rounded_calculation_rent_teuerung = __rounding_value__(
+        final_rent_calculation = __rounding_value__(
             calculation_rent_teuerung)
 
-        return teuerung_result, teuerung_inflation, rounded_calculation_rent_teuerung, inflationRate, affected_date, new_date_formatted_teuerung
+        return from_index, at_index, teuerung_inflation, final_rent_calculation, index_basis, affected_date
     raise BadRequestException('No data found for the inflation')
+
+
+def __get_data_from_manual_input__(payload, total_original, index_basis, affected_date, inflation=100):
+    from_index = payload['inflation']['previous']['index']
+    at_index = payload['inflation']['next']['index']
+
+    teuerung_inflation_not_rounded = __round_inflation_number__(
+        from_index, at_index, inflation)
+    teuerung_inflation = __rounding_value__(teuerung_inflation_not_rounded)
+
+    calculation_rent_teuerung = __rent_calculation__(
+        teuerung_inflation_not_rounded, total_original)
+    final_rent_calculation = __rounding_value__(
+        calculation_rent_teuerung)
+
+    return from_index, at_index, teuerung_inflation, final_rent_calculation, index_basis, affected_date
 
 
 def hypotekarzins_data(payload, rent):
@@ -256,11 +292,34 @@ def hypotekarzins_data(payload, rent):
     toYear = payload['hypoReference']['next']['year']
     toMonth = payload['hypoReference']['next']['month']
     total_original = payload['rent']['rent'] + payload['rent']['extraRoom']
+    input_type = payload['hypoReference']['inputType']
     canton: str = 'CH'
 
     old_date_formatted_hypo, new_date_formatted_hypo = buildDatesInChronologicalOrder(
         fromYear, fromMonth, toYear, toMonth)
 
+    # old_date_formatted_hypo = buildFullDate(fromYear, fromMonth)
+    # new_date_formatted_hypo = buildFullDate(toYear, toMonth)
+
+    if input_type == 'search':
+        return __get_data_hypo_from_search_input__(total_original, canton, old_date_formatted_hypo, new_date_formatted_hypo)
+    return __get_data_hypoe_from_manual_input__(payload, total_original, old_date_formatted_hypo, new_date_formatted_hypo)
+
+
+def __get_data_hypoe_from_manual_input__(payload, total_original, old_date_formatted_hypo, new_date_formatted_hypo):
+    from_interest = payload['hypoReference']['previous']['rate']
+    to_interest = payload['hypoReference']['next']['rate']
+
+    rent_pourcentage_change = __rent_pourcentage_calculation__(
+        from_interest, to_interest)
+    calculation_rent_hypo = __rent_calculation__(
+        total_original, rent_pourcentage_change)
+    final_rent_hypo = __rounding_value__(
+        calculation_rent_hypo)
+    return from_interest, to_interest, rent_pourcentage_change, final_rent_hypo, old_date_formatted_hypo, new_date_formatted_hypo
+
+
+def __get_data_hypo_from_search_input__(total_original, canton, old_date_formatted_hypo, new_date_formatted_hypo):
     # get data based on the old date
     get_index_from_date = __get_index_by_date__(
         canton, old_date_formatted_hypo, 1)
@@ -269,19 +328,17 @@ def hypotekarzins_data(payload, rent):
         canton, new_date_formatted_hypo, 1)
 
     if len(get_index_from_date and get_index_to_date) == 1:
-
-        from_date_interest = get_index_from_date[0].interest
-        to_date_interest = get_index_to_date[0].interest
+        from_interest = get_index_from_date[0].interest
+        to_interest = get_index_to_date[0].interest
 
         rent_pourcentage_change = __rent_pourcentage_calculation__(
-            from_date_interest, to_date_interest)
+            from_interest, to_interest)
 
         calculation_rent_hypo = __rent_calculation__(
             total_original, rent_pourcentage_change)
-        rounded_calculation_rent_hypo = __rounding_value__(
+        final_rent_hypo = __rounding_value__(
             calculation_rent_hypo)
-        return old_date_formatted_hypo, new_date_formatted_hypo, from_date_interest, to_date_interest, rent_pourcentage_change, rounded_calculation_rent_hypo
-
+        return from_interest, to_interest, rent_pourcentage_change, final_rent_hypo, old_date_formatted_hypo, new_date_formatted_hypo
     raise BadRequestException('No data found for the mortgage interest.')
 
 
