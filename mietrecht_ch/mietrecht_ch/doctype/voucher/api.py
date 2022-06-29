6eaacceb6467f6e5568fa03f9c1f6d91ab226957
noxter <david.planchon@liip.ch>
2022-06-29 13:17:25 +0000

    voucher code refactor

--- mietrecht_ch/mietrecht_ch/doctype/voucher/api.py
+++ mietrecht_ch/mietrecht_ch/doctype/voucher/api.py
@@ -2,6 +2,9 @@ from xxlimited import new
 import frappe
 from mietrecht_ch.models.exceptions.mietrechtException import BadRequestException
 from mietrecht_ch.utils.queryExecutor import execute_query
+from mietrecht_ch.models.calculatorResult import CalculatorResult
+from mietrecht_ch.models.calculatorMasterResult import CalculatorMasterResult
+
 
 VOUCHER_TABLE = 'Voucher'
 PAK_TABLE = 'Pak'
@@ -11,31 +14,56 @@ PAK_TABLE = 'Pak'
 def get_voucher_informations(**kwargs):
     voucher_code = kwargs['voucher']
     email = kwargs['email']
-
+    params = {"voucher": voucher_code, "email": email}
     voucher_doctype = frappe.get_doc(VOUCHER_TABLE, voucher_code)
 
     if voucher_doctype:
         voucher_name = voucher_doctype.name
         associated_pak = voucher_doctype.associated_pak
         if associated_pak:
-            return voucher_doctype.associated_pak
+            if email:
+                __save_email_doctype__(email, voucher_doctype)
+            results = {"voucher": voucher_name, "pak": associated_pak}
+            return __calculator_master_result__(params, results)
         # return non_assigned_pak
-        non_assigned_pak = execute_query(
-            """select * from tabPak where associated_voucher = '' or associated_voucher is null ORDER BY label DESC LIMIT 1""")
-        non_assigned_pak_value = non_assigned_pak[0]['label']
-
-        if (email):
-            voucher_doctype.associated_pak = non_assigned_pak_value
-            voucher_doctype.email = email
-            voucher_doctype.save()
-        voucher_doctype.associated_pak = non_assigned_pak_value
-        voucher_doctype.save()
+        non_assigned_pak_value = __non_assigned_pak__()
+        if email:
+            __save_email_doctype__(email, voucher_doctype)
+        __save_associated_pak_to_voucher__(
+            voucher_doctype, non_assigned_pak_value)
         # update the relation in the pak table adding the value of the voucher
-        pak_doctype = frappe.get_doc(PAK_TABLE, non_assigned_pak_value)
-        pak_doctype.associated_voucher = voucher_name
-        pak_doctype.save()
+        __save_data_pak__(voucher_name, non_assigned_pak_value)
+        results = {"voucher": voucher_name, "pak": non_assigned_pak_value}
 
-        return voucher_code.associated_pak
+    return __calculator_master_result__(params, results)
+
+
+def __calculator_master_result__(params, results):
+    calulatorResult = CalculatorResult(results, None)
+    return CalculatorMasterResult(params, [calulatorResult])
+
+
+def __save_associated_pak_to_voucher__(voucher_doctype, non_assigned_pak_value):
+    voucher_doctype.associated_pak = non_assigned_pak_value
+    voucher_doctype.save()
+
+
+def __save_data_pak__(voucher_name, non_assigned_pak_value):
+    pak_doctype = frappe.get_doc(PAK_TABLE, non_assigned_pak_value)
+    pak_doctype.associated_voucher = voucher_name
+    pak_doctype.save()
+
+
+def __save_email_doctype__(email, voucher_doctype):
+    voucher_doctype.email = email
+    voucher_doctype.save()
+
+
+def __non_assigned_pak__():
+    non_assigned_pak = execute_query(
+        """select * from tabPak where associated_voucher = '' or associated_voucher is null ORDER BY label DESC LIMIT 1""")
+    non_assigned_pak_value = non_assigned_pak[0]['label']
+    return non_assigned_pak_value
 
 
 def update_doctype(doctype: str, doctype_name: str, options: dict):
