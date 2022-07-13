0089190f6cb3ad96228f01d2b95a3cf4fa044d47
noxter <david.planchon@liip.ch>
2022-07-13 15:00:37 +0000

    miet-244:Add config doctype
    (cherry picked from commit 8d1885d24a0064d3b51abf8bbcc6e53636deba22)

---
+++ mietrecht_ch/tasks.py
@@ -0,0 +1,42 @@
+import frappe
+
+LIMIT_STOCK = 3
+
+
+def ebook_cron():
+
+    ebook_quantity = __ebook_quantity__()
+
+    if ebook_quantity <= LIMIT_STOCK:
+        __ebook_sendmail__(ebook_quantity)
+
+
+def __ebook_quantity__():
+    ebook_doctype = frappe.db.get_list('Voucher',
+                                       filters={
+                                           'associated_pak': ''
+                                       })
+    number_ebook_available = len(ebook_doctype)
+    return number_ebook_available
+
+
+def __ebook_sendmail__(ebook_quantity):
+
+    email_list_formatted = __get_email_list__()
+    print(email_list_formatted[0])
+    # if email_list_formatted is not None and '':
+    #     frappe.sendmail(
+    #         recipients=email_list_formatted,
+    #         subject=frappe._('E-Book geringe Menge'),
+    #         header=('Der Bestand an E-Books ist gering'),
+    #         template='e-book',
+    #         args=dict(
+    #             ebook_quantity=ebook_quantity
+    #         )
+    #     )
+
+
+def __get_email_list__():
+    email_list = frappe.db.get_single_value('Mietrecht Config', 'config')
+    email_list_formatted = email_list.split(';')
+    return email_list_formatted
