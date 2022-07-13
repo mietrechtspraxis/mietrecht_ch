dd34e9090cdf844da6dc1b461b994e3e35fa855a
Benoit Potty <benoit.potty@liip.ch>
2022-07-13 13:59:08 +0000

    MIET-239: First version of Keywords

--- mietrecht_ch/mietrecht_ch/doctype/stichworte/stichworte.py
+++ mietrecht_ch/mietrecht_ch/doctype/stichworte/stichworte.py
@@ -3,8 +3,23 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 class Stichworte(Document):
-	pass
+
+	def validate(self):
+		self.build_breadcrum()
+		self.update_auto_name()
+
+	def update_auto_name(self):
+		self.name = '{} ({})'.format(self.item_name, self.name)
+
+	def build_breadcrum(self):
+		node = self
+		breadcrum = self.item_name
+		while (node.parent_stichworte != None):
+			parent = frappe.get_doc('Stichworte', node.parent_stichworte)
+			breadcrum = parent.item_name + ' > ' + breadcrum
+			node = parent
+		self.breadcrum = breadcrum
