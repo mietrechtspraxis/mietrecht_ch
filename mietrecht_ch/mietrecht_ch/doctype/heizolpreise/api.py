74452330c3fe3f079a434ed816a45ae4203a42e0
noxter <david.planchon@liip.ch>
2022-07-15 09:05:08 +0000

    Add data validation for heizolpreise

--- mietrecht_ch/mietrecht_ch/doctype/heizolpreise/api.py
+++ mietrecht_ch/mietrecht_ch/doctype/heizolpreise/api.py
@@ -6,65 +6,99 @@ from mietrecht_ch.models.resultTableDescription import ResultTableDescription
 from mietrecht_ch.models.resultRow import ResultRow
 from mietrecht_ch.utils.dateUtils import buildDatesInChronologicalOrder, buildFullDate
 from mietrecht_ch.utils.queryExecutor import execute_query
+from mietrecht_ch.utils.validationUtils import data_empty_value
+from mietrecht_ch.utils.validationTypeUtils import data_type_validation_str
+
 
 @frappe.whitelist(allow_guest=True)
 def get_single_oil_price(quantity, year, month):
+    __validate_data__(quantity, year, month)
     oilPrice = frappe.get_all(
-        'Heizolpreise', 
-        fields = [
-            'monat as month', 
+        'Heizolpreise',
+        fields=[
+            'monat as month',
             """{} as price""".format(quantity),
             """'{}' as quantity""".format(quantity)
-            ], 
-        filters = {
+        ],
+        filters={
             "monat": ("like", buildFullDate(year, month))
         }
-        )
+    )
 
-    calculatorResult = CalculatorResult(oilPrice[0] if oilPrice else None, None)
+    calculatorResult = CalculatorResult(
+        oilPrice[0] if oilPrice else None, None)
 
     return CalculatorMasterResult(
-        {'quantity':quantity, 'year':year, 'month':month},
+        {'quantity': quantity, 'year': year, 'month': month},
         [calculatorResult]
     )
 
+
+def __validate_data__(quantity, year, month):
+    data_empty_value(month, 'month')
+    data_type_validation_str(month, 'month')
+    data_empty_value(year, 'year')
+    data_type_validation_str(year, 'year')
+    data_empty_value(quantity, 'quantity')
+    data_type_validation_str(quantity, 'quantity')
+
+
 @frappe.whitelist(allow_guest=True)
 def get_multiple_oil_price(quantity, fromYear, fromMonth, toYear, toMonth):
-
-    fromFull, toFull = buildDatesInChronologicalOrder(fromYear, fromMonth, toYear, toMonth)
+    __validate_data_for_period__(
+        quantity, fromYear, fromMonth, toYear, toMonth)
+    fromFull, toFull = buildDatesInChronologicalOrder(
+        fromYear, fromMonth, toYear, toMonth)
     oilPrices = frappe.get_all(
         'Heizolpreise',
-        fields = [
-            'monat as month', 
+        fields=[
+            'monat as month',
             """{} as price""".format(quantity),
             """'{} as quantity'""".format(quantity)
-            ], 
-        filters = [
+        ],
+        filters=[
             ["monat", ">=", fromFull],
             ["monat", "<=", toFull],
         ],
-        order_by = 'monat ASC'
+        order_by='monat ASC'
     )
-   
+
     resultTableDescriptions = [
         ResultTableDescription("Monat", "month"),
         ResultTableDescription("Jahr", "year"),
         ResultTableDescription("Preis in CHF", "number"),
     ]
-    
+
     results = []
     if oilPrices:
         for oilPrice in oilPrices:
-            results.append(ResultRow([oilPrice.month.month, oilPrice.month.year, oilPrice.price]))
+            results.append(
+                ResultRow([oilPrice.month.month, oilPrice.month.year, oilPrice.price]))
 
     resultTable = ResultTable(resultTableDescriptions, results)
 
     calculatorResult = CalculatorResult(None, resultTable)
 
     return CalculatorMasterResult(
-        {'quantity':quantity, 'fromYear':fromYear, 'fromMonth':fromMonth, 'toYear':toYear, 'toMonth':toMonth},
+        {'quantity': quantity, 'fromYear': fromYear,
+            'fromMonth': fromMonth, 'toYear': toYear, 'toMonth': toMonth},
         [calculatorResult]
     )
+
+
+def __validate_data_for_period__(quantity, fromYear, fromMonth, toYear, toMonth):
+
+    data_empty_value(quantity, 'location')
+    data_type_validation_str(quantity, 'location')
+    data_empty_value(fromYear, 'fromYear')
+    data_type_validation_str(fromYear, 'fromYear')
+    data_empty_value(fromMonth, 'fromMonth')
+    data_type_validation_str(fromMonth, 'fromMonth')
+    data_empty_value(toYear, 'toYear')
+    data_type_validation_str(toYear, 'toYear')
+    data_empty_value(toMonth, 'toMonth')
+    data_type_validation_str(toMonth, 'toMonth')
+
 
 @frappe.whitelist(allow_guest=True)
 def get_last_data_date():
