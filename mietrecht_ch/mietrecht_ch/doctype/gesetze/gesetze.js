7f1a457e647e677897c1eba129874da49dda16b2
Benoit Potty <benoit.potty@liip.ch>
2022-08-09 12:13:33 +0000

    MIET-259: Improve Gesetze doctype

--- mietrecht_ch/mietrecht_ch/doctype/gesetze/gesetze.js
+++ mietrecht_ch/mietrecht_ch/doctype/gesetze/gesetze.js
@@ -1,8 +1,9 @@
 // Copyright (c) 2022, mietrecht.ch and contributors
 // For license information, please see license.txt
+const DOCTYPE = 'Gesetze';
 
-frappe.ui.form.on('Gesetze', {
-	// refresh: function(frm) {
-
-	// }
+frappe.ui.form.on(DOCTYPE, {
+	after_save: function() {
+		frappe.set_route("List", DOCTYPE);
+	}
 });
--- mietrecht_ch/mietrecht_ch/doctype/gesetze/gesetze.py
+++ mietrecht_ch/mietrecht_ch/doctype/gesetze/gesetze.py
@@ -7,14 +7,17 @@ import frappe
 from frappe.model.document import Document
 
 class Gesetze(Document):
+	# only called on new element creation
 	def autoname(self):
 		self.name = self.__get_name__()
 
+	# only called when updating the element
 	def on_update(self):
+		# check if information used in the name has been changed
 		if (
 			self.has_value_changed('article_number') or
 			self.has_value_changed('paragraph_number') or
-			self.has_value_changed('lit_number') or
+			self.has_value_changed('letter_number') or
 			self.has_value_changed('law_type')
 		):
 			frappe.rename_doc(self.doctype, self.name, self.__get_name__(), force=False, merge=False, ignore_permissions=False, ignore_if_exists=False)
@@ -23,8 +26,8 @@ class Gesetze(Document):
 	def __get_name__(self):
 		name = """Art. {}""".format(self.article_number.strip())
 		if self.paragraph_number != None:
-			name += """ Abs. {} """.format(self.paragraph_number.strip())
-		if self.lit_number != None:
-			name += """ lit. {} """.format(self.lit_number.strip())
+			name += """ Abs. {}""".format(self.paragraph_number.strip())
+		if self.letter_number != None:
+			name += """ lit. {}""".format(self.letter_number.strip())
 		name += """ {}""".format(self.law_type)
 		return name
