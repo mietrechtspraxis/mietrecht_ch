40c771dccfa5d7a77f8e78d82639d82505567e73
Benoit Potty <benoit.potty@liip.ch>
2023-06-05 11:58:39 +0000

    MIET-118: Add editable content

---
+++ mietrecht_ch/mietrecht_ch/doctype/cms_content/api.py
@@ -0,0 +1,14 @@
+import frappe
+from mietrecht_ch.models.exceptions.mietrechtException import BadRequestException
+import json
+
+@frappe.whitelist(allow_guest=True)
+def get_by_key(key = None):
+    if (key == None):
+        raise BadRequestException("Please provide the parameter key")
+
+    content = frappe.get_doc(
+        'CMS Content',
+        key
+    )
+    return content
