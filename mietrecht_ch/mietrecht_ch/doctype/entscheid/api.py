24d4153d6f43b4087fc73d3f2b86cf27632b46f9
noxter <david.planchon@liip.ch>
2023-10-24 08:01:52 +0000

    MIET-299: improve search

--- mietrecht_ch/mietrecht_ch/doctype/entscheid/api.py
+++ mietrecht_ch/mietrecht_ch/doctype/entscheid/api.py
@@ -10,14 +10,21 @@ def healthcheck():
 
 
 @frappe.whitelist(allow_guest=True)
-def search_decision(searchTerm=None):
+def search_decision(searchTerm):
     
     if len(searchTerm) < 4:
         raise BadRequestException('The search term must have at least 4 characters.')
     
-    searchData = frappe.get_all('Entscheide',
+    searchData = frappe.get_all('Entscheid',
+        fields={
+            "description_de",
+            "title_de",
+            "decision_number",
+            "official_collection"
+        },
         filters={
-            ""
+            'type': ['in', 'Entscheid', "Aufsatz"],
+            'title_de': ["like", "%{}%".format(searchTerm)]
         })
     
-    print(searchData)
+    return searchData
