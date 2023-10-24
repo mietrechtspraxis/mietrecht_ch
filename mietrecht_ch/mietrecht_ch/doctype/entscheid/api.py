93727c08d7dd0e60759cdb5118fb7edb1db7c323
noxter <david.planchon@liip.ch>
2023-10-24 12:42:16 +0000

    MIET-299: first api endpoint and search_detailed created

--- mietrecht_ch/mietrecht_ch/doctype/entscheid/api.py
+++ mietrecht_ch/mietrecht_ch/doctype/entscheid/api.py
@@ -10,21 +10,56 @@ def healthcheck():
 
 
 @frappe.whitelist(allow_guest=True)
-def search_decision(searchTerm):
+def search_decision(search=None):
+    if search and len(search) < 4:
+        raise BadRequestException('The search term must have at least 4 characters.')
     
-    if len(searchTerm) < 4:
+    escaped_searched_term =  ["like", f"%{search}%"]  
+  
+    searchData = frappe.get_all('Entscheid',
+        fields={
+            "court",
+            "title_de",
+            "decision_date",
+            "name",
+            "description_de",
+            "decision_number",
+        },
+        filters={
+            'type': ['in', 'Entscheid', "Aufsatz"],
+        },
+        or_filters={
+            'title_de': escaped_searched_term,
+            'description_de': escaped_searched_term,
+            'decision_number': escaped_searched_term,
+            'official_collection': escaped_searched_term
+        }
+    )
+    
+    
+    return searchData
+
+@frappe.whitelist(allow_guest=True)
+def search_detailed(name=None):
+    if name and len(name) < 4:
         raise BadRequestException('The search term must have at least 4 characters.')
     
     searchData = frappe.get_all('Entscheid',
         fields={
-            "description_de",
             "title_de",
+            "decision_date",
+            "court",
             "decision_number",
-            "official_collection"
+            "official_collection",
+            "description_de",
+            "mp_edition",
+            "mp_edition_start_page"
+            
         },
         filters={
             'type': ['in', 'Entscheid', "Aufsatz"],
-            'title_de': ["like", "%{}%".format(searchTerm)]
-        })
+        }
+    )
+    
     
     return searchData
