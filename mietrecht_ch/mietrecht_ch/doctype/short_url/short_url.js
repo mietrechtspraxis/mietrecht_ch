5e1537778bf5d9403ed81d3379431f53a42fe3bf
noxter <david.planchon@liip.ch>
2023-10-25 07:03:02 +0000

    MIET-130: doctype creation

---
+++ mietrecht_ch/mietrecht_ch/doctype/short_url/short_url.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, mietrecht.ch and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Short URL', {
+	// refresh: function(frm) {
+
+	// }
+});
