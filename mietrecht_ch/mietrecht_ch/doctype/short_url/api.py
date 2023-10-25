b72c51c6d5fc0ec7a8983adc6b4716d930ae6c09
noxter <david.planchon@liip.ch>
2023-10-25 10:41:21 +0000

    MIET-130:continue code

--- mietrecht_ch/mietrecht_ch/doctype/short_url/api.py
+++ mietrecht_ch/mietrecht_ch/doctype/short_url/api.py
@@ -4,22 +4,19 @@ from datetime import date
 @frappe.whitelist(allow_guest=True)
 def short_url(uri):
     
-    search_uri =  ["like", f"%{uri}%"]
-    
     current_day = date.today()
     
-    searchData = frappe.get_all('Short URL',
-        fields={
+    search_data = frappe.get_all('Short URL',
+        fields=[
             "redirect",
             "start_date",
             "end_date"
-        },
-        filters={
-            current_day: ['between', ('start_date', 'end_date')]
-        },
-        or_filters={
-            'uri': search_uri,
-        }
+        ],
+        filters=[
+            ['uri', '=', uri],
+            ['start_date', '>=', current_day],
+            ['end_date', '<=', current_day]
+        ],
     )
     
-    return searchData
+    return search_data
