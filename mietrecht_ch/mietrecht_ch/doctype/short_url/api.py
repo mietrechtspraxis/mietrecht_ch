ba339433215d12a4d73dcac7c49719ff2b1d546c
Benoit Potty <benoit.potty@liip.ch>
2023-10-26 12:02:04 +0000

    MIET-130: Refactoring

--- mietrecht_ch/mietrecht_ch/doctype/short_url/api.py
+++ mietrecht_ch/mietrecht_ch/doctype/short_url/api.py
@@ -1,15 +1,14 @@
 import frappe
-import re
 from datetime import date
 from mietrecht_ch.models.exceptions.mietrechtException import BadRequestException
-import datetime 
 
 @frappe.whitelist(allow_guest=True)
 def short_url(uri):
-    
-    current_day = datetime.datetime.now().date()
-    
-    search_data = frappe.get_all('Short URL',
+
+    if len(uri) == 0:
+        raise BadRequestException('uri must be specified')
+            
+    from_database = frappe.get_all('Short URL',
         fields=[
             "redirect",
             "start_date",
@@ -19,17 +18,17 @@ def short_url(uri):
             ['uri', '=', uri],
         ],
     )
-    
-    
-    if search_data is not None and len(search_data) != 0:
-        result_dict = search_data[0]
-        
-        start_date = result_dict.start_date
-        end_date= result_dict.end_date
 
-        if start_date <= current_day <= end_date:
-            return result_dict
+    if from_database is None or len(from_database) == 0:
+        return None
+        
+    object_to_return = from_database[0]
+    
+    if __is_in_range__(object_to_return.start_date, object_to_return.end_date):
+        return object_to_return
 
     return None
-        
-        
+
+def __is_in_range__(start_date, end_date):
+    today = date.today()
+    return (start_date is None or start_date <= today ) and (end_date is None or today <= end_date)
