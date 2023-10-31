750be9254056c9c131a8bd54b177270945ffebae
David Planchon <david.planchon@liip.ch>
2023-10-31 13:03:13 +0000

    MIET-312: Implement user informations to token

--- mietrecht_ch/mietrecht_ch/doctype/user/api.py
+++ mietrecht_ch/mietrecht_ch/doctype/user/api.py
@@ -7,28 +7,27 @@ from mietrecht_ch.models.jwt import JWTGenerator
 login = LoginManager()
 
 @frappe.whitelist(allow_guest=True)
-def get_token(user, expires_in=3600, expire_on=None):
+def auth(user, expires_in=3600, expire_on=None):
     login = LoginManager()
     token = JWTGenerator('secret')
     
+    # Email used to check not name
     if not frappe.db.exists("User", user):
-        raise frappe.ValidationError(_("Invalide User")) 
+        raise frappe.ValidationError(_("Invalide User"))
     
-    payload = {'some': 'payload'}
-    
-    generated_jwt = token.generate_token(payload)
-    decoded_jwt = token.decode_token(generated_jwt)
-    return generated_jwt, decoded_jwt
-
+    # check password first
     if not login.check_password(user, 'mietrecht*'):
         login.fail('Incorrect password', user=user)
-        
-    # return the apu key created, but by hand so need to find a way to generate it by code.
-    #frappe.db.get_value('User', 'Guest', 'api_key')
+    # Get use informations from doctype
+    get_info_user = frappe.db.get_value('User', user, ['name', 'first_name','last_name', 'role_profile_name', 'api_key'], as_dict=1)
+    user_role = get_info_user.role_profile_name
   
-    # integrate jwt to generate a token
-    # maybe find a way to generate a has using hashlib
-    return
+    # Defining the JWT
+    payload = {'user': user, 'role': user_role}
+    generated_jwt = token.generate_token(payload)
+    decoded_jwt = token.decode_token(generated_jwt)
+    return decoded_jwt
+
 
 @frappe.whitelist(allow_guest=True)
 def reset_password(user, email):
@@ -42,6 +41,20 @@ def reset_password(user, email):
         raise BadRequestException("Email don't match.")
         
     # envoie d'email avec link to reset a password.
+    recipients = [
+    'david.planchon@liip.ch',
+    ]      
     
-    if not frappe.db.exists("User", user):
-        raise frappe.ValidationError(_("Invalide User")) 
+    reset_password_link = 'http://reset-password'
+    message = 'Hello, here is you link to reset your password because your email matched our database.'
+
+    frappe.sendmail(
+        recipients=recipients,
+        subject=frappe._('Password reseting'),
+        args=dict(
+            reset_password_link=reset_password_link,
+            message=message,
+        ),
+        header=_('Password resetting')
+    )
+    
