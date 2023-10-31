6691701defd065a81b2eade5aa284bcab4ae1f46
David Planchon <david.planchon@liip.ch>
2023-10-31 09:47:00 +0000

    MIET-312: Create a class based on jwt

--- mietrecht_ch/mietrecht_ch/doctype/user/api.py
+++ mietrecht_ch/mietrecht_ch/doctype/user/api.py
@@ -2,30 +2,33 @@ import frappe
 from frappe import _
 from frappe.auth import LoginManager
 from mietrecht_ch.models.exceptions.mietrechtException import BadRequestException
+from mietrecht_ch.models.jwt import JWTGenerator
 
 login = LoginManager()
 
 @frappe.whitelist(allow_guest=True)
 def get_token(user, expires_in=3600, expire_on=None):
+    login = LoginManager()
+    token = JWTGenerator('secret')
     
-  if not frappe.db.exists("User", user):
-    raise frappe.ValidationError(_("Invalide User")) 
+    if not frappe.db.exists("User", user):
+        raise frappe.ValidationError(_("Invalide User")) 
+    
+    payload = {'some': 'payload'}
+    
+    generated_jwt = token.generate_token(payload)
+    decoded_jwt = token.decode_token(generated_jwt)
+    return generated_jwt, decoded_jwt
 
-
+    if not login.check_password(user, 'mietrecht*'):
+        login.fail('Incorrect password', user=user)
+        
+    # return the apu key created, but by hand so need to find a way to generate it by code.
+    #frappe.db.get_value('User', 'Guest', 'api_key')
   
-  if not login.check_password(user, 'mietrecht*'):
-      login.fail('Incorrect password', user=user)
-      
-  # return the apu key created, but by hand so need to find a way to generate it by code.
-  #frappe.db.get_value('User', 'Guest', 'api_key')
-  
-  return login.authenticate(user, 'mietrecht*')
-
-
-  
-  # integrate jwt to generate a token
-  # maybe find a way to generate a has using hashlib
-  return
+    # integrate jwt to generate a token
+    # maybe find a way to generate a has using hashlib
+    return
 
 @frappe.whitelist(allow_guest=True)
 def reset_password(user, email):
