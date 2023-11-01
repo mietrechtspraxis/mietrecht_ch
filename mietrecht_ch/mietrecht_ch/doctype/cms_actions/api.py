370907fea6651f34bb63722ccd70da6aaefcd26e
Benoit Potty <benoit.potty@liip.ch>
2023-11-01 12:33:02 +0000

    MIET-277: Add backend URL in the return

--- mietrecht_ch/mietrecht_ch/doctype/cms_actions/api.py
+++ mietrecht_ch/mietrecht_ch/doctype/cms_actions/api.py
@@ -1,5 +1,10 @@
+import json
 import frappe
 from mietrecht_ch.models.exceptions.mietrechtException import BadRequestException
+from pprint import pprint
+from urllib.parse import urlparse
+
+from mietrecht_ch.utils.hostUtils import get_website_url
 
 @frappe.whitelist(allow_guest=True)
 def get_cms_actions(action_group_key):
@@ -15,6 +20,9 @@ def get_cms_actions(action_group_key):
     """)
 
     actions = []
+
+    site_url = get_website_url()
+
     for db_action in db_actions:
         action = {
             'key': db_action[0],
@@ -22,7 +30,7 @@ def get_cms_actions(action_group_key):
             'url': db_action[2],
             'description': db_action[3],
             'isInternal': db_action[4],
-            'fileUrl': db_action[5],
+            'fileUrl': f"{site_url}{db_action[5]}" if db_action[5] is not None else None,
         }
         actions.append(action)
 
