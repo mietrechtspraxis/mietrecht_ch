e7db47f9ea46bd59d64b1b512032a9f0d541f7d0
David Planchon <david.planchon@liip.ch>
2023-11-02 13:43:31 +0000

    MIET-338: handle json body request

--- mietrecht_ch/mietrecht_ch/doctype/antwort_auf_das_formular/api.py
+++ mietrecht_ch/mietrecht_ch/doctype/antwort_auf_das_formular/api.py
@@ -1,5 +1,35 @@
 import frappe
+import json
+from frappe import _
 
-@frappe.whitelist(allow_guest=True)
-def get_response_form_data():
+@frappe.whitelist(allow_guest=True, methods=["POST"])
+def create_response_form():
+    if frappe.request.method == "POST":
+        request_data = frappe.local.request.data
+        request_data_str = request_data.decode('utf-8')
+
+        # Parse the string as JSON
+        request_data = json.loads(request_data_str)
+
+        # Access and process the data from the request body
+        # data_value = request_data.get("key")
+        return request_data
+      
+    [
+        {
+            "genre": "String(dropdown)" - "Frau" | "Herr",
+            "first_name": "String",
+            "last_name": "String",
+            "company": "String",
+            "street": "String",
+            "company_number": "Interger",
+            "zip_code": "String",
+            "address_complement": "String",
+            "email": "String",
+            "annotation": "String",
+            "additional_data": "String",
+            "type": "String(dropdown)" - "Bestellformular “Kurs- und Seminarprogramm”" | "Kontakt" | "Andere" 
+        }   
+    ]
+    
     pass
