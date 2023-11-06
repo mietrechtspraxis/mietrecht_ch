f9ca2788c4d54e5449c6db641d5fa8ed2f108ed0
David Planchon <david.planchon@liip.ch>
2023-11-06 13:10:29 +0000

    MIET-338: Improve code and handle specific errors

--- mietrecht_ch/mietrecht_ch/doctype/antwort_auf_das_formular/api.py
+++ mietrecht_ch/mietrecht_ch/doctype/antwort_auf_das_formular/api.py
@@ -6,56 +6,61 @@ from frappe import _
 @frappe.whitelist(allow_guest=True, methods=["POST"])
 def create_response_form():
     request = is_post_method()
-    
-    if request is not None and len(request) != 0:   
-        first_name = request.get('first_name')
-        last_name = request.get('last_name')
-        email = request.get('email')
-        _type = request.get('type')
+
+    if request is not None and len(request) != 0:
+
+        if __validate_fields__(request) is None:
+           return __insert_new_document__(request)
+            
+        return __validate_fields__(request)
         
-        required_fields = {
-            'first_name': first_name,
-            'last_name': last_name,
-            'email': email,
-            '_type': _type    
-         }
         
-        __is_not_empty__(required_fields)
-        
-        doc = frappe.new_doc('Antwort auf das Formular')
-        doc.first_name = first_name
-        doc.last_name = last_name
-        doc.email = email
-        doc.email = _type
-        doc.insert(ignore_permissions=True)
-        
-        # start creating the new_doc based on received data
-        return 'all good'
     return BadRequestException('The form cannot be empty.')
     
-    [
-        {
-            "genre": "String(dropdown)" - "Frau" | "Herr",
-            "first_name": "String",
-            "last_name": "String",
-            "company": "String",
-            "street": "String",
-            "company_number": "Interger",
-            "zip_code": "String",
-            "address_complement": "String",
-            "email": "String",
-            "annotation": "String",
-            "additional_data": "String",
-            "type": "String(dropdown)" - "Bestellformular “Kurs- und Seminarprogramm”" | "Kontakt" | "Andere" 
-        }   
-    ]
-    
-def  __is_not_empty__(fields):
-    errors = []    
-    for k, v in fields.items():
-        if v is "" and len(v) == 0:
-            errors.append({ k : f'{k} cannot be empty.'})
 
+def __insert_new_document__(request):
+    first_name = request.get('first_name')
+    last_name = request.get('last_name')
+    firma = request.get('firma')
+    street = request.get('street')
+    zip_code = request.get('zip_code')
+    company_number = request.get('company_number')
+    
+    doc = frappe.new_doc('Antwort auf das Formular')
+    doc.first_name = first_name
+    doc.last_name = last_name
+    doc.firma = firma
+    doc.zip_code = zip_code
+    doc.street = street
+    doc.company_number = company_number
+    doc.insert(ignore_permissions=True)
+    
+    # frappe.sendmail(
+    #     ["david.planchon@liip.ch"],
+    #     message="## hello, *bro*",
+    #     attachments=[{"file_url": "/files/hello.png"}],
+    #     as_markdown=True
+    #     )
+    
+    # start creating the new_doc based on received data
+    return {'response': 'Die Bestellung eines Kursprogramms wurde erfolgreich gesendet.'}
+    
+def __validate_fields__(request):
+    errors = []
+    for k in [request]:
+        first_name = k['first_name']
+        last_name = k['last_name']
+        firma = k['firma']
+        street = k['street']
+        company_number = k['company_number']
+        zip_code = k['zip_code']
+        
+        if (first_name == "" and len(first_name) == 0 or last_name == "" and len(last_name) == 0) and firma == "" and len(firma) == 0 :
+            errors.append({'error' : 'Please add either a First Name and Last Name or a Firma value.'})
+            
+        if (street == "" and len(street) == 0 or zip_code == "" and len(zip_code) == 0) and (company_number == "" and len(company_number) == 0):  
+            errors.append({'error' : 'Please add either a Street and zip code or a valid Company Number.'})
+    
     if errors:
         return errors
 
@@ -66,6 +71,4 @@ def is_post_method():
 
         request_data = json.loads(request_data_str)
 
-        # Access and process the data from the request body
-        # data_value = request_data.get("key")
         return request_data
