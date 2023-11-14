de29a6b43c6589be9b8b5cdabaaa2b730844858c
Benoit Potty <benoit.potty@liip.ch>
2023-11-14 09:38:02 +0000

    MIET-312: Simplify login using cookies

--- mietrecht_ch/mietrecht_ch/doctype/user/api.py
+++ mietrecht_ch/mietrecht_ch/doctype/user/api.py
@@ -1,50 +1,39 @@
 import frappe
-import datetime
 from frappe import _
 from frappe.auth import LoginManager
-from mietrecht_ch.models.jwt import JWTGenerator
-
+import json
 
 @frappe.whitelist(allow_guest=True)
-def auth(user, pwd, expires_in=3600, expire_on=None):
-
+def auth(user, pwd):
     try:
         login = LoginManager()
-        login.user = user
-        login.authenticate(user, pwd)
+        login.authenticate(user=user, pwd=pwd)
         login.post_login()
-    except:
         frappe.clear_messages()
+    except:
         frappe.local.response["message"] = {
             "success_key":0,
             "message":"Authentication Error!"
         }
         return 
     
-    user_role = __get_user_info__(user)
+    user_roles = frappe.get_roles(frappe.session.user)
+    mp_roles = [role for role in user_roles if role.startswith("mp_web")]
+    frappe.local.cookie_manager.set_cookie("mp_roles", json.dumps(mp_roles))
+    del frappe.local.response["home_page"]
+    del frappe.local.response["full_name"]
     
-    generated_jwt = __generate_jwt__(user, user_role)
-        
     response = frappe.response["message"] = {
         "success_key":1,
         "message":"Authentication success",
-        "sid":frappe.session.sid,
-        "jwt":generated_jwt,
     }
     
-    # Return the current user session.
     return response
 
-def __generate_jwt__(user, user_role):
-    token = JWTGenerator('secret')
-    expire_time = datetime.datetime.utcnow() + datetime.timedelta(hours=1)
-    payload = {'user': user, 'role': user_role, 'exp': expire_time}
-    generated_jwt = token.generate_token(payload)
-    print(generated_jwt)
-    return generated_jwt
-
-def __get_user_info__(user):
-    get_info_user = frappe.db.get_value('User', user, ['name', 'first_name','last_name', 'role_profile_name', 'api_key'], as_dict=1)
-    user_role = get_info_user.role_profile_name
-    return user_role
-
+@frappe.whitelist(allow_guest=True)
+def logout():
+    LoginManager().logout()
+    frappe.local.response["message"]= {
+        'success_key': 1,
+        "message":"Logout success",
+    }
