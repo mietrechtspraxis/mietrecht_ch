a5f06943cb3d729365f68123d21fc70f3a0ffa5b
David Planchon <david.planchon@liip.ch>
2023-11-23 13:39:46 +0000

    MIET-223: Logic for Abo

--- mietrecht_ch/mietrecht_ch/doctype/antwort_auf_das_formular/api.py
+++ mietrecht_ch/mietrecht_ch/doctype/antwort_auf_das_formular/api.py
@@ -1,15 +1,20 @@
 import frappe
-from frappe.core.doctype.user.user import sign_up
 from mietrecht_ch.models.exceptions.mietrechtException import BadRequestException
 import json
 from frappe import _
+from datetime import datetime
+from frappe.config import get_modules_from_all_apps
+import secrets
+import string
+
 
 MESSAGE_ERROR = { 'created': False, 'cmsErrorKey' : 'SHOP_ERROR_BESTELLUNG' }
+MP_ABO_ROLE = "mp_web_user_abo"
 
 @frappe.whitelist(allow_guest=True, methods=['POST'])
 def create_form_answer():
     try:
-        request = return_json_data()
+        request = __return_json_data__()
 
         if request is not None and len(request) != 0:
             if not __validate_fields__(request):
@@ -20,7 +25,7 @@ def create_form_answer():
         
     return BadRequestException('The form cannot be empty.')
     
-def get_address_data(request):
+def __get_address_data__(request):
     delivery_address = request.get('delivery_address')
     
     return {
@@ -34,9 +39,30 @@ def get_address_data(request):
         'additional_info': delivery_address.get('additional_info'),
     }
 
-    
 def create_form_response(request):
-    # Main form data
+    # Create doctype structure
+    first_name, last_name, email, different_delivery_address, doc = __create_doctype_structure__(request)
+
+    # Add delivery address to the doctype if different address is checked
+    if different_delivery_address:
+        __add_different_address_to_doctype__(request, doc)
+
+    data = json.loads(request.get('data'))
+    abo_data = data.get('abo')
+
+    if abo_data and abo_data.startswith(("PERI-ABO-", "PERI-3DAY%", "Probe-Abo")):
+        try:
+            __create_base_user__(first_name, last_name, email)   
+            __add_role_mp__(email)
+            return {'userCreated': True}
+        except:
+            return MESSAGE_ERROR
+    
+    doc.insert(ignore_permissions=True)
+
+    return { 'created': True, 'orderNumber': doc.name }
+
+def __create_doctype_structure__(request):
     billing_address = request.get('billing_address')
     gender = billing_address.get('gender')
     first_name = billing_address.get('first_name')
@@ -70,105 +96,69 @@ def create_form_response(request):
         'remarks': remarks,
         'different_delivery_address': different_delivery_address,
     })
+    
+    return first_name,last_name,email,different_delivery_address,doc
 
-    # Create or update delivery address
-    if different_delivery_address:
-        delivery_data = get_address_data(request)
-        doc.update({
+def __add_different_address_to_doctype__(request, doc):
+    delivery_data = __get_address_data__(request)
+    doc.update({
             'delivery_' + key: value for key, value in delivery_data.items()
         })
 
-    data = json.loads(request.get('data'))
-    abo_data = data.get('abo')
-    full_name = f"{first_name + ' ' + last_name}"
-
-    if abo_data == "PERI-ABO-%" or abo_data == "PERI-3DAY%" or abo_data == "Probe-Abo":
-        # User creation account
-        # sign_up(email, full_name, None)
-
-        # upadte_sql = """
-        #     UPDATE `tabModule Def` as md
-        #     INNER JOIN `tabUser` as u ON u.email = "benoit.potty@liip.ch"
-        #     SET u.enabled = 0
-        #     WHERE u.enabled = 1
-        #      """
-
+def __add_role_mp__(email):
+    user_modification = frappe.get_doc('User', email)
+    user_modification.flags.ignore_permissions = True
+    user_modification.remove_roles()
         
-        
-        # result = frappe.db.sql(upadte_sql, as_dict=True)
-        # return
+    user_modification.add_roles(MP_ABO_ROLE)
+    user_modification.save()
+    return user_modification
 
-        module_name = 'Accounts'
-
-            # Fetch the user
-        user = frappe.get_doc({
+def __create_base_user__(first_name, last_name, email):
+    user = frappe.get_doc({
             "doctype":"User",
             "email": email,
             "first_name": first_name,
             "last_name": last_name,
             "user_type": "Website User"
             })
-            # Remove the module permission for the specified module
-        modules = frappe.get_all("Module Def", fields=[])
-        
-        return modules
-        # Print module information
-        for module in modules:
-            print(f"Module Name: {module.module_name}, Label: {module.label}")
-        user.flags.ignore_permissions = True
-        return user
-        user.save()
-        return
-
-        # Remove the module from the roles assigned to the user
-        for role in user_doc.get("roles"):
-            role_doc = frappe.get_doc("Role", role.role)
-            if module_name in role_doc.get("module_def_whitelist"):
-                return True
-                role_doc.remove("module_def_whitelist", {"module_def": module_name})
-                role_doc.save()
-                print(role_doc)
-
-        # Clear cache to apply changes
-        frappe.clear_cache()
-        return
-        # update_query = """
-        #     UPDATE `tabModule Def`
-        #     SET enabled = '0'
-        #     WHERE email IN (SELECT * FROM `tabUser` WHERE email = 'benoit.potty@liip.ch');
-        # """
-        # result = frappe.db.sql(update_query, as_dict=True)
-        # return result
-
-        user = frappe.get_doc({
-        "doctype":"User",
-        "email": email,
-        "first_name": first_name,
-        "last_name": last_name,
-        "user_type": "Website User"
-		})
-
-
-        
-        user.flags.ignore_permissions = True
-        user.flags.ignore_password_policy = True
-        user.insert()
     
+    user.flags.ignore_permissions = True
+    user.flags.ignore_password_policy = True
+    user.block_modules = __blocked_modules__(email)
+    user.insert()
 
-        user_modification = frappe.get_doc('User', email)
-        user_modification.flags.ignore_permissions = True
-        user_modification.remove_roles()
-        user_modification.add_roles('mp_web_user_abo')
-        return user_modification.save()
-        
-    
+def __generate_random_code__(length=10):
+    return ''.join(secrets.choice(string.ascii_letters + string.digits) for _ in range(length))
 
-        
-        
-        
-    doc.insert(ignore_permissions=True)
+def __remove_modules_to_user__(index, module, date, email):
+    random_code = __generate_random_code__()
+    module_doctype = frappe.get_doc({
+        "creation": date,
+        "docstatus": 0,
+        "doctype": "Block Module",
+        "idx": index + 1,
+        "modified": date,
+        "modified_by": "Administrator",
+        "module": module['module_name'],
+        "name": random_code,
+        "owner": "Administrator",
+        "parent": email,
+        "parentfield": "block_modules",
+        "parenttype": "User"
+    })
+    return module_doctype
 
-    return { 'created': True, 'orderNumber': doc.name }
+def __blocked_modules__(email):
+    all_modules = get_modules_from_all_apps()
+    blocked_modules = []
+    date = datetime.now()
+
+    for index, module in enumerate(all_modules):
+        module_doctype = __remove_modules_to_user__(index, module, date, email)
+        blocked_modules.append(module_doctype)
+
+    return blocked_modules
 
 def __validate_address_fields__(address):
     last_name = address.get('last_name', '')
@@ -205,7 +195,7 @@ def __validate_fields__(request):
 
     return None
 
-def return_json_data():
+def __return_json_data__():
     if frappe.get_request_header('Content-Type') != 'application/json':
         frappe.throw("Invalid content type. Expected application/json.", title="Bad Request")
         
