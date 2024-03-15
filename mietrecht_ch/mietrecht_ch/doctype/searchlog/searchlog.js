094079ef340f1b109e4c6de0b337f151ff36340f
Christoph Laszlo <christoph.laszlo@mieterverband.ch>
2024-03-15 21:27:05 +0100

    adding doctype for logging search

---
+++ mietrecht_ch/mietrecht_ch/doctype/searchlog/searchlog.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, mietrecht.ch and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('SearchLog', {
+	// refresh: function(frm) {
+
+	// }
+});
