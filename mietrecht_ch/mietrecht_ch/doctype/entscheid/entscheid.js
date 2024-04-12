daca6fd32f6ac668604a976305e0698fa8721de7
Christoph Laszlo <christoph.laszlo@mieterverband.ch>
2024-04-12 14:30:33 +0200

    Entscheide Umbenennen zulassen, Website-öffnen-Buttons

--- mietrecht_ch/mietrecht_ch/doctype/entscheid/entscheid.js
+++ mietrecht_ch/mietrecht_ch/doctype/entscheid/entscheid.js
@@ -3,18 +3,18 @@
 
 frappe.ui.form.on('Entscheid', {
 	open_id_old: function(frm) {
-		var openlink = window.open('https://www.mietrecht.ch/db/mr_entsch_show.php?id='+frm.doc.id_old, '_blank', 'popup=yes');
+		var openlink = window.open('https://legacy.mietrecht.ch/db/mr_entsch_show.php?id='+frm.doc.id_old, '_blank', 'popup=yes');
 	},
 	open_web: function(frm) {
 		var baseUrl
-		if (window.location.hostname == "mietrecht.localhost") {
+		if (window.location.hostname == "localhost") {
 			baseUrl = 'http://localhost:4200/entscheide/';
 		} else if (window.location.hostname == 'mp.libracore.ch' ) {
-			baseUrl = 'https://staging2.mietrecht.ch/entscheide/';
+			baseUrl = 'https://www.mietrecht.ch/entscheide/';
 		} else if (window.location.hostname == 'mp-test.libracore.ch') {
 			baseUrl = 'https://staging2.mietrecht.ch/entscheide/';
 		} else {
-			baseUrl = 'https://mietrecht.ch/entscheide/';
+			baseUrl = 'https://www.mietrecht.ch/entscheide/';
 		}
 		var openlink = window.open(baseUrl + frm.doc.name, '_blank', 'popup=yes');
 	 }
